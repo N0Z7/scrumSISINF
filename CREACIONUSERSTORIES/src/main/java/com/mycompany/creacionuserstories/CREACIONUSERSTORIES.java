cc982dd0d8c3d8882bfcca307613da688a4a2757
Abel <202000212@est.umss.edu>
2023-03-31 23:55:34 -0400

    Conexión a firebase de Abel Pacheco

---
+++ CREACIONUSERSTORIES/src/main/java/com/mycompany/creacionuserstories/CREACIONUSERSTORIES.java
@@ -0,0 +1,10 @@
+//import static com.mycompany.sprint.ConexionFB.db;
+package com.mycompany.creacionuserstories;
+
+public class CREACIONUSERSTORIES {
+
+    public static void main(String[] args) {
+        ConexionFB conectar = new ConexionFB();
+        conectar.conectarFb();
+    }
+}
